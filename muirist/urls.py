ac49007e0a8ef133007d639dd80fd136acf40cbb
Ben Gregory <JBGregoryPro@protonmail.com>
2021-12-15 11:24:18 -0600

    list viewset works

--- muirist/urls.py
+++ muirist/urls.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
-from muiristapi.views import register_user, login_user, user_profile, ParkView, SnippetView
+from muiristapi.views import register_user, login_user, user_profile, ParkView, SnippetView, ListView
 from rest_framework import routers
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'parks', ParkView, 'park')
 router.register(r'snippets', SnippetView, 'snippet')
-
+router.register(r'lists', ListView, 'list')
 urlpatterns = [
     path('', include(router.urls)),
     path('register', register_user),
--- muiristapi/models/lists.py
+++ muiristapi/models/lists.py
@@ -4,5 +4,7 @@ from django.db import models
 
 class List(models.Model):
 
+
     muirist = models.ForeignKey("Muirist", on_delete=models.CASCADE)
     park = models.ForeignKey("Park", on_delete=models.CASCADE)
+    
