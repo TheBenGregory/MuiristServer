5f38dd3a145e2df9304b0c2b96513678ff08dde9
Ben Gregory <JBGregoryPro@protonmail.com>
2022-01-05 11:44:56 -0600

    fixed content value

--- muiristapi/models/snippets.py
+++ muiristapi/models/snippets.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Snippet(models.Model):
 
-    title = models.CharField(max_length=30)
+    title = models.CharField(max_length=100)
     content = models.CharField(max_length=120)
     muirist = models.ForeignKey("Muirist", on_delete=models.CASCADE)
     park = models.ForeignKey("Park", on_delete=models.CASCADE)
